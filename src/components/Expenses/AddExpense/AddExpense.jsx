8c1e26adc163e48ff00acb5afd9fa72812c6df1b
Akhee123 <mabuirmelah@gmail.com>
2023-03-06 13:57:03 +0200

    Connect category with expenses

--- src/components/Expenses/AddExpense/AddExpense.jsx
+++ src/components/Expenses/AddExpense/AddExpense.jsx
@@ -1,42 +1,48 @@
-import {  AppContext } from "../../../App";
+import { AppContext } from "../../../App";
 import { useContext } from "react";
 
 function AddExpense() {
+  const context = useContext(AppContext);
 
-    const context = useContext(AppContext);
-    
-    function submitHandler(event){
-        event.preventDefault();
+  function submitHandler(event) {
+    event.preventDefault();
 
-        const form = event.target;
-        const formData = new FormData(form);
-        const formObj = Object.fromEntries(formData.entries());
-        context.setExpenses( prevState => {
-            return [...prevState, formObj];
-        });
-    }
+    const form = event.target;
+    const formData = new FormData(form);
+    const formObj = Object.fromEntries(formData.entries());
+    context.setExpenses((prevState) => {
+      return [...prevState, formObj];
+    });
+  }
 
-    return (
-        <>
-            <h1>Add AddExpense</h1>
-            <form onSubmit={submitHandler}>
-                <label htmlFor="item">Item</label>
-                <input type="text" name="item" />
+  return (
+    <>
+      <h1>Add AddExpense</h1>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="item">Item</label>
+        <input type="text" name="item" />
 
-                <label htmlFor="date">Date</label>
-                <input type="date" name="date" />
+        <label htmlFor="date">Date</label>
+        <input type="date" name="date" />
 
-                <label htmlFor="amount">Amount</label>
-                <input type="number" name="amount" />
+        <label htmlFor="amount">Amount</label>
+        <input type="number" name="amount" />
 
-                <label htmlFor="category">Category</label>
-                <input type="text" name="category" />
+        <label htmlFor="category">Category</label>
+        <select name="category" id="category">
+          {context.category.map((element, index) => {
+            return (
+              <option key={index} value={element.category}>
+                {element.category}
+              </option>
+            );
+          })}
+        </select>
 
-                <button type="submit">Add Expense</button>
-
-            </form>
-        </>
-    );
+        <button type="submit">Add Expense</button>
+      </form>
+    </>
+  );
 }
 
 export default AddExpense;
